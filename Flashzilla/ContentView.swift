536b26f6300782af54c187da56ae262c989fa23b
Peter Barclay <peter@peter-barclay.com>
2019-12-18 08:18:19 +0000

    How to use gestures in SwiftUI - gesture sequence

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -5,22 +5,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var offset = CGSize.zero
+
+    @State private var isDragging = false
+
     var body: some View {
-        VStack {
-            Text("Hello, World!")
-                .onTapGesture {
-                    print("Text tapped")
+        let dragGesture = DragGesture()
+            .onChanged { value in self.offset = value.translation }
+            .onEnded { _ in
+                withAnimation {
+                    self.offset = .zero
+                    self.isDragging = false
                 }
-        }
-        .simultaneousGesture(
-            TapGesture()
-                .onEnded { _ in
-                    print("VStack tapped")
+            }
+
+        let tapGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    self.isDragging = true
                 }
-        )
+            }
+
+        let combined = tapGesture.sequenced(before: dragGesture)
+
+        return Circle()
+            .fill(Color.red)
+            .frame(width: 64, height: 64)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
