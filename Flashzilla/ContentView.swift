f1e916f6760099852feb02a35e0172581d59bb6f
Peter Barclay <peter@peter-barclay.com>
2019-12-18 08:14:50 +0000

    How to use gestures in SwiftUI - gesture priority

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -5,22 +5,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var currentAmount: Angle = .degrees(0)
-    @State private var finalAmount: Angle = .degrees(0)
-
     var body: some View {
-        Text("Hello, World!")
-            .rotationEffect(currentAmount + finalAmount)
-            .gesture(
-                RotationGesture()
-                    .onChanged { angle in
-                        self.currentAmount = angle
-                    }
-                    .onEnded { angle in
-                        self.finalAmount += self.currentAmount
-                        self.currentAmount = .degrees(0)
-                    }
-            )
+        VStack {
+            Text("Hello, World!")
+                .onTapGesture {
+                    print("Text tapped")
+                }
+        }
+        .highPriorityGesture(
+            TapGesture()
+                .onEnded { _ in
+                    print("VStack tapped")
+                }
+        )
     }
 }
 
