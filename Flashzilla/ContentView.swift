61d6171fe524e1c5f385c8561a92f32cfaec058d
Peter Barclay <peter@peter-barclay.com>
2019-12-18 08:07:12 +0000

    How to use gestures in SwiftUI - LongPressGesture pressing closure

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -7,7 +7,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
-            .onLongPressGesture(minimumDuration: 2) {
+            .onLongPressGesture(minimumDuration: 1, pressing: { inProgress in
+                print("In progress: \(inProgress)!")
+            }) {
                 print("Long pressed!")
             }
     }
