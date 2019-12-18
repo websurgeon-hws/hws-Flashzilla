218a517905b4f2ae1e4920a927bccea163c60ecf
Peter Barclay <peter@peter-barclay.com>
2019-12-18 08:38:30 +0000

    Disabling user interactivity - contentShape to cover Spacer view

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,22 +8,15 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(Color.blue)
-                .frame(width: 300, height: 300)
-                .onTapGesture {
-                    print("Rectangle tapped!")
-                }
-
-            Circle()
-                .fill(Color.red)
-                .frame(width: 300, height: 300)
-                .contentShape(Rectangle()) // shape used for tappable area
-                .onTapGesture {
-                    print("Circle tapped!")
-                }
+        VStack {
+            Text("Hello")
+            Spacer().frame(height: 100)
+            Text("World")
         }
+            .contentShape(Rectangle())
+            .onTapGesture {
+                print("VStack tapped!")
+            }
     }
 }
 
