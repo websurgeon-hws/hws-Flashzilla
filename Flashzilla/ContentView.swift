c06b1806b8e0ce9327bca725abb08ba8b0190155
Peter Barclay <peter@peter-barclay.com>
2019-12-19 08:18:08 +0000

    Triggering events repeatedly using a timer
    If you need to keep time strictly then leaving off the tolerance parameter will make your time as accurate as possible, but please note that even without any tolerance the Timer class is still “best effort” – the system makes no guarantee it will execute precisely.

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -2,20 +2,23 @@
 //  Copyright © 2019 Peter Barclay. All rights reserved.
 //
 
-import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
     
+    @State private var counter = 0
+
     var body: some View {
-        VStack {
-            Text("Hello")
-            Spacer().frame(height: 100)
-            Text("World")
-        }
-            .contentShape(Rectangle())
-            .onTapGesture {
-                print("VStack tapped!")
+        Text("Hello, World!")
+            .onReceive(timer) { time in
+                if self.counter == 5 {
+                    self.timer.upstream.connect().cancel()
+                } else {
+                    print("The time is now \(time)")
+                }
+
+                self.counter += 1
             }
     }
 }
